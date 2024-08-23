a3a4dbebac9b67d8e322cbe9c1d3d363bb69e363
ZedSoftOfficial <132583827+ZedSoftOfficial@users.noreply.github.com>
2024-08-24 00:56:17 +0330

    Update loi.sh

--- loi.sh
+++ loi.sh
@@ -1,201 +1,29 @@
 #!/bin/bash
 
-echo "What should I do?"
-echo "1) 6to4"
-echo "2) 6to4 multi server (1 Outside 2 Iran)"
-echo "3) Remove tunnels"
-echo "4) Enable BBR"
-echo "5) Fix Whatsapp Time"
-echo "6) Optimize"
-echo "7) Install x-ui"
-echo "8) Change NameServer"
-echo "9) Disable IPv6 - After server reboot IPv6 is activated"
-read -p "Select an option (1, 2, 3, 4, 5, 6, 7, 8, or 9): " server_choice
+# نمایش منوی اصلی
+echo "Select an option:"
+echo "1) 6to4 multi server (1 outside 2 Iran)"
+read -p "Select an option (1): " main_option
 
-setup_rc_local() {
-    FILE="/etc/rc.local"
-    commands="$1"
-
-    # Ensure the file exists and is executable, or create and set it to exit 0
-    if [ ! -f "$FILE" ]; then
-        echo -e '#!/bin/bash\n\nexit 0' | sudo tee "$FILE" > /dev/null
-    fi
-
-    # Remove any existing commands before adding new ones
-    sudo sed -i '/^exit 0/d' "$FILE"
-
-    # Add new commands
-    echo "$commands" | sudo tee -a "$FILE" > /dev/null
-
-    # Ensure 'exit 0' is at the end of the file
-    if ! sudo tail -n 1 "$FILE" | grep -q '^exit 0'; then
-        echo "exit 0" | sudo tee -a "$FILE" > /dev/null
-    fi
-
-    sudo chmod +x "$FILE"
-    echo "Commands added to /etc/rc.local"
-    echo "Commands executed immediately."
-    eval "$commands"
-}
-
-fix_whatsapp_time() {
-    commands="sudo timedatectl set-timezone Asia/Tehran"
-    setup_rc_local "$commands"
-    echo "Whatsapp time fixed to Asia/Tehran timezone."
-}
-
-optimize() {
-    USER_CONF="/etc/systemd/user.conf"
-    SYSTEM_CONF="/etc/systemd/system.conf"
-    LIMITS_CONF="/etc/security/limits.conf"
-    SYSCTL_CONF="/etc/sysctl.d/local.conf"
-    TEMP_USER_CONF=$(mktemp)
-    TEMP_SYSTEM_CONF=$(mktemp)
-
-    add_line_if_not_exists() {
-        local file="$1"
-        local line="$2"
-        local temp_file="$3"
-
-        if [ -f "$file" ]; then
-            cp "$file" "$temp_file"
-            if ! grep -q "$line" "$file"; then
-                sed -i '/^\[Manager\]/a '"$line" "$temp_file"
-                sudo mv "$temp_file" "$file"
-                echo "Added '$line' to $file"
-            else
-                echo "The line '$line' already exists in $file"
-                rm "$temp_file"
-            fi
-        else
-            echo "$file does not exist."
-            rm "$temp_file"
-        fi
-    }
-
-    add_line_if_not_exists "$USER_CONF" "DefaultLimitNOFILE=1024000" "$TEMP_USER_CONF"
-    add_line_if_not_exists "$SYSTEM_CONF" "DefaultLimitNOFILE=1024000" "$TEMP_SYSTEM_CONF"
-
-    if [ -f "$LIMITS_CONF" ]; then
-        cat <<EOF | sudo tee -a "$LIMITS_CONF"
-* hard nofile 1024000
-* soft nofile 1024000
-root hard nofile 1024000
-root soft nofile 1024000
-EOF
-        echo "Added limits to $LIMITS_CONF"
-    else
-        echo "$LIMITS_CONF does not exist."
-    fi
-
-    cat <<EOF | sudo tee "$SYSCTL_CONF"
-# max open files
-fs.file-max = 1024000
-EOF
-    echo "Added sysctl settings to $SYSCTL_CONF"
-
-    sudo sysctl --system
-    echo "Sysctl changes applied."
-}
-
-install_x_ui() {
-    echo "Choose the version of x-ui to install:"
-    echo "1) alireza"
-    echo "2) MHSanaei"
-    read -p "Select an option (1 or 2): " xui_choice
-
-    if [ "$xui_choice" -eq 1 ]; then
-        bash <(curl -Ls https://raw.githubusercontent.com/alireza0/x-ui/master/install.sh)
-        echo "alireza version of x-ui installed."
-    elif [ "$xui_choice" -eq 2 ]; then
-        bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
-        echo "MHSanaei version of x-ui installed."
-    else
-        echo "Invalid option. Please select 1 or 2."
-    fi
-}
-
-disable_ipv6() {
-    commands=$(cat <<EOF
-sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
-sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
-sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
-EOF
-)
-
-    setup_rc_local "$commands"
-    echo "IPv6 has been disabled."
-}
-
-handle_six_to_four() {
-    echo "Choose the type of server:"
-    echo "1) Outside"
-    echo "2) Iran1"
-    echo "3) Iran2"
-    read -p "Select an option (1 or 2): " six_to_four_choice
-
-    if [ "$six_to_four_choice" -eq 1 ]; then
-        read -p "Enter the IP outside: " ipkharej
-        read -p "Enter the IP Iran: " ipiran
-
-        commands=$(cat <<EOF
-ip tunnel add 6to4_To_IR mode sit remote $ipiran local $ipkharej
-ip -6 addr add 2002:480:1f10:e1f::2/64 dev 6to4_To_IR
-ip link set 6to4_To_IR mtu 1480
-ip link set 6to4_To_IR up
-
-ip -6 tunnel add GRE6Tun_To_IR mode ip6gre remote 2002:480:1f10:e1f::1 local 2002:480:1f10:e1f::2
-ip addr add 10.10.10.2/30 dev GRE6Tun_To_IR
-ip link set GRE6Tun_To_IR mtu 1436
-ip link set GRE6Tun_To_IR up
-EOF
-)
-
-        setup_rc_local "$commands"
-        echo "Commands executed for the outside server."
-
-    elif [ "$six_to_four_choice" -eq 2 ]; then
-        read -p "Enter the IP Iran: " ipiran
-        read -p "Enter the IP outside: " ipkharej
-
-        commands=$(cat <<EOF
-ip tunnel add 6to4_To_KH mode sit remote $ipkharej local $ipiran
-ip -6 addr add 2002:480:1f10:e1f::1/64 dev 6to4_To_KH
-ip link set 6to4_To_KH mtu 1480
-ip link set 6to4_To_KH up
-
-ip -6 tunnel add GRE6Tun_To_KH mode ip6gre remote 2002:480:1f10:e1f::2 local 2002:480:1f10:e1f::1
-ip addr add 10.10.10.1/30 dev GRE6Tun_To_KH
-ip link set GRE6Tun_To_KH mtu 1436
-ip link set GRE6Tun_To_KH up
-
-sysctl net.ipv4.ip_forward=1
-iptables -t nat -A PREROUTING -p tcp --dport 22 -j DNAT --to-destination 10.10.10.1
-iptables -t nat -A PREROUTING -j DNAT --to-destination 10.10.10.2
-iptables -t nat -A POSTROUTING -j MASQUERADE
-EOF
-)
-
-        setup_rc_local "$commands"
-        echo "Commands executed for the Iran server."
-
-    else
-        echo "Invalid option. Please select 1 or 2."
-    fi
-}
-
-handle_six_to_four_multi_server() {
+# اگر کاربر گزینه 1 را انتخاب کرد، ادامه دهید
+if [ "$main_option" -eq 1 ]; then
+    echo "Which server is this?"
     echo "1) Outside"
     echo "2) Iran1"
     echo "3) Iran2"
     read -p "Select an option (1, 2, or 3): " server_option
 
     if [ "$server_option" -eq 1 ]; then
+        # برای سرور Outside
         read -p "Enter the IP Outside: " ipkharej1
         read -p "Enter the IP Iran1: " ipiran1
         read -p "Enter the IP Iran2: " ipiran2
 
-        commands=$(cat <<EOF
+        # ایجاد محتوای جدید برای فایل rc.local
+        cat <<EOL > /etc/rc.local
+#!/bin/bash
+
+# تنظیمات تونل برای اولین سرور ایران
 ip tunnel add 6to4_To_IR1 mode sit remote $ipiran1 local $ipkharej1
 ip -6 addr add 2002:480:1f10:e1f::2/64 dev 6to4_To_IR1
 ip link set 6to4_To_IR1 mtu 1480
@@ -206,6 +34,7 @@ ip addr add 10.10.10.2/30 dev GRE6Tun_To_IR1
 ip link set GRE6Tun_To_IR1 mtu 1436
 ip link set GRE6Tun_To_IR1 up
 
+# تنظیمات تونل برای دومین سرور ایران
 ip tunnel add 6to4_To_IR2 mode sit remote $ipiran2 local $ipkharej1
 ip -6 addr add 2009:480:1f10:e1f::2/64 dev 6to4_To_IR2
 ip link set 6to4_To_IR2 mtu 1480
@@ -215,52 +44,68 @@ ip -6 tunnel add GRE6Tun_To_IR2 mode ip6gre remote 2009:480:1f10:e1f::1 local 20
 ip addr add 10.10.11.2/30 dev GRE6Tun_To_IR2
 ip link set GRE6Tun_To_IR2 mtu 1436
 ip link set GRE6Tun_To_IR2 up
-EOF
-)
 
-        setup_rc_local "$commands"
+exit 0
+EOL
+
+        chmod +x /etc/rc.local
         echo "Configuration for Outside saved to /etc/rc.local and the file has been made executable."
 
     elif [ "$server_option" -eq 2 ]; then
+        # برای سرور Iran1
         read -p "Enter the IP Outside: " ipkharej1
         read -p "Enter the IP Iran1: " ipiran1
+
+        # گرفتن پورت‌ها
         read -p "Enter the ports (comma separated, e.g., 443,8080): " ports
         IFS=',' read -r -a port_array <<< "$ports"
 
-        commands=$(cat <<EOF
-ip tunnel add 6to4_To_IR mode sit remote $ipkharej1 local $ipiran1
-ip -6 addr add 2002:480:1f10:e1f::1/64 dev 6to4_To_IR
-ip link set 6to4_To_IR mtu 1480
-ip link set 6to4_To_IR up
+        # ایجاد محتوای جدید برای فایل rc.local
+        cat <<EOL > /etc/rc.local
+#!/bin/bash
 
-ip -6 tunnel add GRE6Tun_To_IR mode ip6gre remote 2002:480:1f10:e1f::2 local 2002:480:1f10:e1f::1
-ip addr add 10.10.10.1/30 dev GRE6Tun_To_IR
-ip link set GRE6Tun_To_IR mtu 1436
-ip link set GRE6Tun_To_IR up
+ip tunnel add 6to4_To_KH mode sit remote $ipkharej1 local $ipiran1
+ip -6 addr add 2002:480:1f10:e1f::1/64 dev 6to4_To_KH
+ip link set 6to4_To_KH mtu 1480
+ip link set 6to4_To_KH up
 
+ip -6 tunnel add GRE6Tun_To_KH mode ip6gre remote 2002:480:1f10:e1f::2 local 2002:480:1f10:e1f::1
+ip addr add 10.10.10.1/30 dev GRE6Tun_To_KH
+ip link set GRE6Tun_To_KH mtu 1436
+ip link set GRE6Tun_To_KH up
+
+# فعال کردن فورواردینگ IPv4
 sysctl net.ipv4.ip_forward=1
-EOF
-)
+EOL
 
+        # اضافه کردن قوانین iptables بر اساس تعداد پورت‌ها
         for i in "${!port_array[@]}"; do
-            commands+="iptables -t nat -A PREROUTING -p tcp --dport ${port_array[$i]} -j DNAT --to-destination 10.10.10.1
-"
+            echo "iptables -t nat -A PREROUTING -p tcp --dport ${port_array[$i]} -j DNAT --to-destination 10.10.10.1" >> /etc/rc.local
         done
 
-        commands+="iptables -t nat -A POSTROUTING -j MASQUERADE
-EOF
-"
+        # اضافه کردن دستور POSTROUTING و خروج
+        cat <<EOL >> /etc/rc.local
+iptables -t nat -A POSTROUTING -j MASQUERADE 
 
-        setup_rc_local "$commands"
+exit 0
+EOL
+
+        chmod +x /etc/rc.local
         echo "Configuration for Iran1 saved to /etc/rc.local and the file has been made executable."
 
     elif [ "$server_option" -eq 3 ]; then
+        # برای سرور Iran2
         read -p "Enter the IP Outside: " ipkharej1
         read -p "Enter the IP Iran2: " ipiran2
+
+        # گرفتن پورت‌ها
         read -p "Enter the ports (comma separated, e.g., 443,8080): " ports
         IFS=',' read -r -a port_array <<< "$ports"
 
-        commands=$(cat <<EOF
+        # ایجاد محتوای جدید برای فایل rc.local
+        cat <<EOL > /etc/rc.local
+#!/bin/bash
+
 ip tunnel add 6to4_To_KH mode sit remote $ipkharej1 local $ipiran2
 ip -6 addr add 2009:480:1f10:e1f::1/64 dev 6to4_To_KH
 ip link set 6to4_To_KH mtu 1480
@@ -271,86 +116,30 @@ ip addr add 10.10.11.1/30 dev GRE6Tun_To_KH
 ip link set GRE6Tun_To_KH mtu 1436
 ip link set GRE6Tun_To_KH up
 
+# فعال کردن فورواردینگ IPv4
 sysctl net.ipv4.ip_forward=1
-EOF
-)
+EOL
 
+        # اضافه کردن قوانین iptables بر اساس تعداد پورت‌ها
         for i in "${!port_array[@]}"; do
-            commands+="iptables -t nat -A PREROUTING -p tcp --dport ${port_array[$i]} -j DNAT --to-destination 10.10.11.1
-"
+            echo "iptables -t nat -A PREROUTING -p tcp --dport ${port_array[$i]} -j DNAT --to-destination 10.10.11.1" >> /etc/rc.local
         done
 
-        commands+="iptables -t nat -A POSTROUTING -j MASQUERADE
-EOF
-"
+        # اضافه کردن دستور POSTROUTING و خروج
+        cat <<EOL >> /etc/rc.local
+iptables -t nat -A POSTROUTING -j MASQUERADE 
 
-        setup_rc_local "$commands"
+exit 0
+EOL
+
+        chmod +x /etc/rc.local
         echo "Configuration for Iran2 saved to /etc/rc.local and the file has been made executable."
 
     else
         echo "Invalid server option selected. Exiting."
         exit 1
     fi
-}
-
-remove_tunnels() {
-    echo "Removing tunnels..."
-    sudo ip tunnel del 6to4_To_IR1
-    sudo ip tunnel del GRE6Tun_To_IR1
-    sudo ip tunnel del 6to4_To_IR2
-    sudo ip tunnel del GRE6Tun_To_IR2
-    sudo ip tunnel del 6to4_To_KH
-    sudo ip tunnel del GRE6Tun_To_KH
-
-    # Clear the /etc/rc.local file and set it to only exit 0
-    echo -e '#!/bin/bash\n\nexit 0' | sudo tee /etc/rc.local > /dev/null
-
-    echo "Tunnels removed and /etc/rc.local has been cleared and set to exit 0."
-}
-
-enable_bbr() {
-    echo "Enabling BBR..."
-    sudo bash -c 'echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf'
-    sudo bash -c 'echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf'
-    sudo sysctl -p
-    echo "BBR enabled."
-}
-
-change_nameserver() {
-    echo "Changing NameServer..."
-    sudo bash -c 'echo "nameserver 8.8.8.8" > /etc/resolv.conf'
-    echo "NameServer changed to 8.8.8.8."
-}
-
-case $server_choice in
-    1)
-        handle_six_to_four
-        ;;
-    2)
-        handle_six_to_four_multi_server
-        ;;
-    3)
-        remove_tunnels
-        ;;
-    4)
-        enable_bbr
-        ;;
-    5)
-        fix_whatsapp_time
-        ;;
-    6)
-        optimize
-        ;;
-    7)
-        install_x_ui
-        ;;
-    8)
-        change_nameserver
-        ;;
-    9)
-        disable_ipv6
-        ;;
-    *)
-        echo "Invalid option. Please select 1, 2, 3, 4, 5, 6, 7, 8, or 9."
-        ;;
-esac
+else
+    echo "Invalid main option selected. Exiting."
+    exit 1
+fi
